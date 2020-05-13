f24242e6bcf42c5b7b9adcb98caacc4173e547af
Luis Reyes <luisreyesxv@yahoo.com>
2020-05-13 05:02:42 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -26,7 +26,7 @@ def won? (board)
       if win_combination.detect{ |position_check| position_check == true} == [true, true, true]
         return win_combination
       else
-        return false  
+        return false
     end
 #&& position_taken?(board,win_index_1) == true && position_taken?(board,win_index_2) == true && position_taken?(board,win_index_2) == true
 
