a6922af717eb8e1856ec0d44bb3a4f5bf026f00a
Luis Reyes <luisreyesxv@yahoo.com>
2020-05-13 05:25:55 +0000

    Automatically backed up by Learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -16,19 +16,10 @@ WIN_COMBINATIONS =[
 ]
 
 def won? (board)
-
   WIN_COMBINATIONS.detect do |win_combination|
     win_index_1 = win_combination[0]
     win_index_2 = win_combination[1]
     win_index_3 = win_combination[2]
-
     board[win_index_1] == board[win_index_2] && board[win_index_2] == board[win_index_3] && position_taken?(board,win_index_1)
-
-
-
   end
-
-
-
-
 end
